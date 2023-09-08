74c016593cd14ab0e9184a4ec2eddddee189226a
Bob <31620450+bjoziasse@users.noreply.github.com>
2023-09-08 16:51:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,24 @@ document.addEventListener('DOMContentLoaded', function() {
             initialView: 'dayGridMonth',
             events: events,
             eventClick: function(info) {
-                alert('Reservation: ' + info.event.title + '\n\n' + info.event.description);
+                document.getElementById('modalTitle').textContent = info.event.title;
+                document.getElementById('modalDescription').textContent = info.event.description;
+                var modal = document.getElementById("myModal");
+                var span = document.getElementsByClassName("close")[0];
+                
+                modal.style.display = "block";
+                
+                // When the user clicks on <span> (x), close the modal
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
+                
+                // When the user clicks anywhere outside of the modal, close it
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
             }
         });
 
@@ -28,23 +45,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-eventClick: function(info) {
-    document.getElementById('modalTitle').textContent = info.event.title;
-    document.getElementById('modalDescription').textContent = info.event.description;
-    var modal = document.getElementById("myModal");
-    var span = document.getElementsByClassName("close")[0];
-    
-    modal.style.display = "block";
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}

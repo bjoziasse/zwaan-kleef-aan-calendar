6472c35b1f1ffeba6ba96ab97a62ea9e59c878f7
Bob <31620450+bjoziasse@users.noreply.github.com>
2023-09-08 16:50:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,24 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error("Error fetching data from reservations.json");
     });
 });
+
+eventClick: function(info) {
+    document.getElementById('modalTitle').textContent = info.event.title;
+    document.getElementById('modalDescription').textContent = info.event.description;
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
+    
+    modal.style.display = "block";
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
